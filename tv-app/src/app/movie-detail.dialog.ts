328a50a74f6fb76c5d9e7b7902afff7ca2e524e9
Charles Jones <charlesjones1088@gmail.com>
2017-04-21 08:07:00 -0600

    Getting dialog to look better

--- tv-app/src/app/movie-detail.dialog.ts
+++ tv-app/src/app/movie-detail.dialog.ts
@@ -7,11 +7,24 @@ import { Movie } from './Movie';
     selector: 'movie-detail-dialog',
     templateUrl: './movie-detail.dialog.html',
     styles: [
-        `.full-plot {
-            width: 510px;
+        `.dialog-text-width {
+            width:510px;
+        }`,
+        `.title {
+            margin-top: 0;
+            margin-bottom: 0;
         }`
     ]
 })
 export class MovieDetailDialog {
     public movie: Movie
+
+    hideMovie() {
+        let hiddenMovies = [];
+        if(localStorage.getItem('hidden-movies')) {
+            let hiddenMovies = JSON.parse(localStorage.getItem('hidden-movies'));
+        }
+        hiddenMovies.push(this.movie.imdb);
+        localStorage.setItem('hidden-movies', JSON.stringify(hiddenMovies));
+    }
 }
